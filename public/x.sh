6450412a07d078de1ae30b9e401c20cc49045f70
Ignacio Anaya <ignacio.anaya89@gmail.com>
2020-10-12 17:39:07 -0300

    Add script

---
+++ public/x.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "🚨 Tene cuidado con lo que copias y pegas en la terminal"
+echo "😈 Esto podria haber hecho algo peligroso"
+echo ""
+echo "🙅  Nunca lo hagas a menos que sea seguro y confies en la fuente"
+
+echo ""
+echo ""
+
+echo "💀😵 ️"
